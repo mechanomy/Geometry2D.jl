096add7c60c4cef3eba7075f5bb17d2058294986
Ben Conrad <ben@benconrad.net>
2022-08-02 16:44:29 -0500

    test docs action

--- docs/make.jl
+++ docs/make.jl
@@ -10,5 +10,33 @@ using Geometry2D
 
 makedocs(
   sitename="Geometry2D.jl",
-  modules=[Geometry2D] 
+  modules=[Geometry2D],
+  root = joinpath(dirname(pathof(Geometry2D)), "..", "docs"),
+  source = "src",
+  build = "build",
+  clean=true,
+  doctest=true,
+  repo = "github.com/mechanomy/Geometry2D.jl.git",
+  draft=false,
+  checkdocs=:all,
+  # linkcheck=true, fails to find internal links to bookmarks..
   )
+
+deploydocs(
+  root = joinpath(dirname(pathof(Geometry2D)), "..", "docs"),
+  target = "build",
+  dirname = "",
+  repo = "github.com/mechanomy/Geometry2D.jl.git",
+  branch = "gh-pages",
+  # deps = nothing | <Function>,
+  # make = nothing | <Function>,
+  devbranch = "main",
+  devurl = "dev",
+  versions = ["stable" => "v^", "v#.#", devurl => devurl],
+  forcepush = false,
+  deploy_config = auto_detect_deploy_system(),
+  push_preview = false,
+  repo_previews = repo,
+  branch_previews = branch,
+)
+
